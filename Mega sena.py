4db190c29a56d36f351ae4c90e0e7a938261a921
Christian Santos <110914214+Chr1st14N94@users.noreply.github.com>
2024-06-26 17:11:05 -0300

    Update Mega sena.py

--- Mega sena.py
+++ Mega sena.py
@@ -1,8 +1,6 @@
 import random
-
 for a in range(1):
-
     for c in range(1,6):
-        n1 = random.randint(1, 81)
+        n1 = random.randint(1, 61)
         print(f'{n1}',end="  ")
     print(' \n')
